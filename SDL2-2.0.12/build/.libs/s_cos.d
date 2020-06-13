957fc8109c32818f3fb846bc5d2cd418ae620beb
pseudo2020 <20pseudo20@gmail.com>
2020-06-13 21:58:49 +0100

    ~

---
+++ SDL2-2.0.12/build/.libs/s_cos.d
@@ -0,0 +1,10 @@
+build/s_cos.lo: \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/libm/s_cos.c \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/libm/math_libm.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/libm/../SDL_internal.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/libm/../dynapi/SDL_dynapi.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/libm/../dynapi/SDL_dynapi_overrides.h \
+ include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+ include/close_code.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/libm/math_private.h \
+ include/SDL_endian.h include/SDL_stdinc.h include/SDL_config.h
---
+++ SDL2-2.0.12/build/SDL_waylandtouch.d
@@ -0,0 +1,25 @@
+build/SDL_waylandtouch.lo: \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/SDL_waylandtouch.c \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/../../SDL_internal.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/../../dynapi/SDL_dynapi.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/../../dynapi/SDL_dynapi_overrides.h \
+ include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+ include/close_code.h include/SDL_log.h include/SDL_stdinc.h \
+ include/SDL_config.h include/SDL_mouse.h include/SDL_error.h \
+ include/SDL_video.h include/SDL_pixels.h include/SDL_endian.h \
+ include/SDL_rect.h include/SDL_rwops.h include/SDL_surface.h \
+ include/SDL_blendmode.h include/SDL_keyboard.h include/SDL_keycode.h \
+ include/SDL_scancode.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/SDL_waylandtouch.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/SDL_waylandvideo.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/SDL_waylanddyn.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/SDL_waylandsym.h \
+ gen/wayland-client-protocol.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/../../events/SDL_touch_c.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/../../events/../SDL_internal.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/../../events/../../include/SDL_touch.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/../../events/../../include/SDL_stdinc.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/../../events/../../include/SDL_error.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/../../events/../../include/SDL_video.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/../../events/../../include/begin_code.h \
+ /home/pseudo/code/c++/pistachio/SDL2-2.0.12/src/video/wayland/../../events/../../include/close_code.h
